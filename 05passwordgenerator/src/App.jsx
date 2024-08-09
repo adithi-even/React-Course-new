eb1ae6c3ffeb487bc2d02c0db3492bad7f144b4c
adithi-even <adithigopal1409@gmail.com>
2024-08-09 07:38:21 +0530

    added coments

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [password, setPassword] = useState({})
 
   // useRef hook
-  const passwordRef = useRef(null)
+  const passwordRef = useRef(null)  //this ref and its value are talking with the variable password ref in the above 48th line
+  //since they are conected now we have a reference of an element which we can manipulate  i.e., input eleement and from the variable copyPasswordToClipboard we are telling the useREf hook what to do 
 
   const passwordGenerator = useCallback(()=>{
     let pass = ""
@@ -18,18 +19,22 @@ function App() {
     if (charAllowed) str += "+_)(*&^%$#@!{}[]-"
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length +1)     
-      pass += str.charAt (char )
+      let char = Math.floor(Math.random() * str.length +1)    //this line adds the single character every time the loop runs to check u can log it in the browser 
+      console.log(char,i );
+      
+      // pass += str.charAt(char)  // see line 95-116 for example
+      // console.log(pass);
+      
     }
     setPassword(pass)
 
   } , [length, numberAllowed,charAllowed,setPassword])    // setpassword is for optimization if you dont want to give dont give
 
   const copyPasswordToClipboard = useCallback(()=>{
-    passwordRef.current?.select()
-    passwordRef.current?.setSelectionRange(0,20)
-    window.navigator.clipboard.writeText(password)
-  },[password])
+    passwordRef.current?.select() //selecting the password if there any 
+    passwordRef.current?.setSelectionRange(0,20) //seleeeecting password on the basis of range 
+    window.navigator.clipboard.writeText(password) //we are writing the password to clipboard
+  },[password]) // the dependency we are using here is password coz we only use the state variable which we can relate to and which is in this case we want to copy the password so we are currently only interested in password.
 
   useEffect(()=>{passwordGenerator()},[length,numberAllowed,charAllowed,passwordGenerator])
 
@@ -40,16 +45,30 @@ function App() {
 
       {/*  */}
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-        <input type="text" value={password} className='outline-none w-full py-1 px-3 '  placeholder='Password' readOnly ref={passwordRef} />
+        <input type="text" value={password} className='outline-none w-full py-1 px-3 '  placeholder='Password' readOnly 
+        ref={passwordRef} />  
         <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
 
         {/* slider */}
         <div className='flex items-center gap-x-1'>
-          <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e)=>{setLength(e.target.value)}} />
+
+          <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e)=>{setLength(e.target.value)}} 
+          
+          //Event Object: e contains details about the event, including e.target.value, which holds the new value from the input field.
+
+          //The input field's value is controlled by the 'length' state.
+
+          //slider i.e., range will not work until we give onchange function .
+
+          //e.target.value provides the new value from the input field, which is passed to setLength to update the state.
+           />
+
           <label >Length:{length} </label>
         </div>
+
+        
 
         {/* number check box */}
         <div className='flex items-center gap-x-1'>
@@ -85,3 +104,31 @@ function App() {
 }
 
 export default App
+
+
+
+// this is an example for line 24 ==>       pass += str.charAt (char )
+
+//1st iteration
+// pass = pass+ str.chatAt(char)
+// pass = pass+ str.chatAt(3)
+// pass = pass+ str.chatAt(3)
+// pass = pass+ 'D'
+// ' ' =pass+'D'
+// ' '=' ' + 'D'
+// pass = 'D'
+
+
+//2nd iteration
+// //coz the previous value of the pass is 'D'
+// pass = pass+ str.chatAt(char)
+// pass = pass+ str.chatAt()
+// pass = pass+ str.chatAt(9)
+// pass = pass+ 'J'
+// 'D' =pass+'J'
+// 'D'='D' + 'J'
+// 'D' = 'DJ'
+// pass = 'DJ'
+
+// |||ly for every iteration the single seperate charcter will get appended 
+
