2bd1538ecf2c286ae32a89e5adea2b1851d849e0
adithi-even <adithigopal1409@gmail.com>
2024-09-11 08:13:25 +0530

    added script.js

---
+++ fetchApi-JS/script.js
@@ -0,0 +1,25 @@
+function first() {
+    console.log(1)
+  }
+  
+  function second(callback) {
+      setTimeout(() => {
+          console.log(2)
+          callback()
+        }, 0)
+  }
+  
+  function third() {
+    console.log(3)
+  }
+
+
+first()
+second(third)
+  
+
+const promise1 = new promise((resolve, reject)=>{
+    
+})
+
+
