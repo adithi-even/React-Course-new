5cfd8bbbc630674fb8d3bce48bcfbec360ff3139
adithi-even <adithigopal1409@gmail.com>
2024-09-12 08:23:32 +0530

    changed ...

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, createRoutesFromElements, Route,   RouterProvider} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
@@ -35,16 +35,17 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 const  router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element= {<Home/>} />
+    //opening tag 
+    <Route path='/' element= {<Home/>}> 
+    
       <Route path='about' element= {<About/>} />
       <Route path='contact' element= {<Contact/>} />
-      <Route path='user/:userid' element= {<User/>} />
+      <Route path='user/:userid' element= {<User/>}  />
       <Route
        loader={githubInfoLoader}      
        path='github'
        element= {<Github/>} />
-    </Route>
+    </Route>//closing tag
 
   )
 )
