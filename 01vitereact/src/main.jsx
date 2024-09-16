6d2816e137f457cf381c0e4ec9c7ea9c2a486e2c
adithi-even <adithigopal1409@gmail.com>
2024-09-16 08:43:14 +0530

    added some coments

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -21,22 +21,27 @@ function MyApp(){
 // }
 
 const anotherElement = (
-    <a href="https://google.com" target='_blank'>Visit google</a>
+    <a href="https://google.com" target='_blank'>hiiiiiiiii</a>
 )
 
 
 
-const anotherUser = "chai aur react"
+// const anotherUser = "chai aur react"
 
-const reactElement = React.createElement(
-    'a',
-    {href: 'https://google.com',target: '_blank' },
-    'click me to visit google',
-    anotherElement
+const reactElement = React.createElement( //this is NOT AN OBJECT , we are giving element like 1st 2nd 3rd similar to the (tags, id ,class inside the parenthesis of the queryselector)
+    'a', //1st
+    {href: 'https://google.com',target: '_blank' },//2nd
+    'visit google heyyyyyyyy',//3rd
+    anotherElement //after all the above we give variable
 )
+
+function Funct(){
+    return reactElement
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
+    // Funct()
     reactElement
   
 )
--- customReact/customreact.js
+++ customReact/customreact.js
@@ -16,13 +16,26 @@ function customRender(reactElement , container){
 }
 const reactElement = {
     type:'a',
-    props: {
+    props: {  //also called as attributes
         href : ' https://www.google.com' ,
         target :'_blank '
 
     }, 
     children :' Click Me to visit google '
 }
-const mainContainer = document.querySelector('#root')
+const mainContainer = document.querySelector('#root') // here we are getting the div with the id=root from html 
  
 customRender(reactElement,mainContainer)
+
+
+// step 1: const mainContainer , (even though its in the last line)
+
+//step 2 : const reactElement  , since its a react build by me all the elements in my react will look like this only , if anybody wants to access this then they have to use it like this only 
+
+//step 3: now we have to add the newly create element insiede the root  i.e., of a type:'a' which is an <a> tag into the div
+
+//step 4:if we have to do that in the step 3 . we need a function which takes the 2 params they are (which element needs to get injected , which element will get injected by the previous param)
+
+//step 5 :function customRender(reactElement ,  container)
+
+//step 6: creating a dom element
