d6f86ef9c67f3354f16de8eb813255876bee4c07
adithi-even <adithigopal1409@gmail.com>
2024-09-29 03:04:35 +0530

    understood the redux and updated some comments for the easy readability

--- 11reduxtoolkittodo/src/features/todo/todoSlice.js
+++ 11reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import {createSlice,nanoid} from '@reduxjs/toolkit'  //nanoid generates the unique id thats it 
 
 const initialState = {
-    todos:[{id:1, text: "Hello Wordl"}]
+    todos:[{id:1, text: "Hello Wordl"}] //list of todo will be in future currently we have created an array which initially have only 1 todo after that it may be multiple  todos , so the name todos .
 }
 
 export const todoSlice = createSlice({
@@ -9,11 +9,11 @@ export const todoSlice = createSlice({
     initialState,
     reducers: {
         addTodo: () =>{
-            const todo = {
-                id:nanoid(), 
+            const todo = { //here its an individual todo 
+                id:nanoid(),  //here instead of using the static id :1 , we useing the dynamic id
                 text: action.payload
             }
-            state.todos.push(todo)
+            state.todos.push(todo)//this is the updated version of the spread operator and adding the updated count value as inteh the chat gpt version of images i have in my pc is the previous method of updating the state which we also has done similar work in the context API and now here we are using the different method , you can access the image in the react screenshot folder for the understanding purpose only and how it works and not to implement it in the actual code , when you are creating the project by using the @redux/toolkit and react-redux . 
         } ,
         removeTodo : (state ,action) => {
             state.todos = state.todos.filter((todos) =>todos.id !== action.payload)
