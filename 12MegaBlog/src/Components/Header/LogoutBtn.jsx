859fc2e3611992128402d4a17397f08e61554f98
adithi-even <adithigopal1409@gmail.com>
2024-10-07 19:18:58 +0530

    12

--- 12MegaBlog/src/Components/Header/LogoutBtn.jsx
+++ 12MegaBlog/src/Components/Header/LogoutBtn.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-im
+import {useDispatch} from 'react-redux' 
+import authService from '../../appwrite/auth'
+import {logout} from '../../store/authSlice'
+
+
+//Handling the Promise: The ".then" method is used to define what happens once the logout operation completes successfully. If the logout is successful, the function passed to ".then" will execute
+
 function LogoutBtn() {
+
+  const dispatch = useDispatch()
+  const logoutHandler = () => {
+    authService.logout().then(()=>{
+      dispatch(logout())
+    })
+    
+  }
   return (
-    <div>LogoutBtn</div>
+    <button
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
   )
 }
 
-export default LogoutBtnx
+export default LogoutBtn
