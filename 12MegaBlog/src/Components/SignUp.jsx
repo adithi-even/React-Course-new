daba2b6d71a4eed819c24fd373a6ccf6684e7c0c
adithi-even <adithigopal1409@gmail.com>
2024-10-15 08:34:49 +0530

    sign UP component

--- 12MegaBlog/src/Components/SignUp.jsx
+++ 12MegaBlog/src/Components/SignUp.jsx
@@ -1,33 +1,32 @@
-import React , {useState} from 'react'
+import React from 'react'
+import authService from '../appwrite/auth'
 import {Link , useNavigate} from 'react-router-dom'
-import {login as authLogin } from '../store/authSlice'
+import {login  } from '../store/authSlice'
 import {Button , Input , Logo } from './Index'
 import { useDispatch } from 'react-redux'
-import authService from '../appwrite/auth'
 import {useForm} from 'react-hook-form'
 
-function Login() {
+function SignUp() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const {register , handleSubmit} = useForm()
   const [error , setError] = useState("")
-  
-  const login = async(data) => { //TODO: UNDERSTAND THIS LOGIC  
-    console.log(data);
-    
-    setError("") //when ever we start the process of login , set the error to null or setting the error to empty or clean or empty the error
+
+  const Create = async(data) =>{
+    setError("")
     try {
-      const session = await authService.login(data) //if session is there then the user is logged in if the sesssion is not there there then the user is not logged in .
-      if(session){
+      const userData = await authService.createAccount(data)
+      if(userData){
         const userData = await authService.getCurrentUser()
-        if(userData)dispatch(authLogin(userData));
-        navigate("/") //after the user logged in we can programatically send the user to other pages through (navigate) not like link coz for link we have to click on that but here we are sending the user programatically to other page through (navigate)
+        if(userData)dispatch(login(userData));
+        navigate("/")
       }
-      
+
     } catch (error) {
-      setError(error.message) // if there is any error we are going to store it in the state , and after that we will condittionally render it , if we wanna show the error to the user or not .
+        setError(error.message)
     }
   }
+  
   return (
     <div>
 
@@ -37,7 +36,7 @@ function Login() {
                         <Logo width="100%" />
                     </span>
         </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+        <h2 className="text-center text-2xl font-bold leading-tight">Sign up to your account</h2>
         <p className="mt-2 text-center text-base text-black/60">
                     Don&apos;t have any account?&nbsp;
                     <Link
@@ -52,8 +51,16 @@ function Login() {
         
 
         <form //handleSubmit has become a keyword which comes from the  useForm , and whenever the form gets submit the handle submit is an event which is gonna run 
-         onSubmit={handleSubmit(login)} className='mt-8'>
+         onSubmit={handleSubmit(Create)} className='mt-8'>
           <div  className='space-y-5'>
+            <Input
+            label="Name:"
+            placeholder = "Enter your Name "
+            type = "name"
+            {...register("name"),{
+              required : true 
+            }} ///to notify which input is this for ex:this particular input is the email input so we have named it email , for others we will name as password etc
+            />
             <Input
             label="Email:"
             placeholder = "Enter your Email "
@@ -79,7 +86,7 @@ function Login() {
             type='Submit'
             className='w-full'
             >
-              LogIn
+              Sign UP
             </Button>
 
           </div>
@@ -90,7 +97,4 @@ function Login() {
   )
 }
 
-export default Login
-
-
- 
+export default SignUp
