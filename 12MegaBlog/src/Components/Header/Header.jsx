278d44affe066ca22d2f72294219953ed711cb03
adithi-even <adithigopal1409@gmail.com>
2024-12-18 20:35:10 +0530

    e

--- 12MegaBlog/src/Components/Header/Header.jsx
+++ 12MegaBlog/src/Components/Header/Header.jsx
@@ -59,7 +59,7 @@ function Header() {
                 <button
                 onClick={() => navigate(item.slug)}
                 className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                >{item.name}</button> // Button text
+                >{item.name}</button> 
               </li>
             ) : null
             )}
